77b4cd92e4f5dde28cbf8a536b18123e2bdee444
J Valentin <jerber.valentin@gmail.com>
2025-04-18 21:31:11 -0500

    finalizado·

--- src/NumeroAleatorio.java
+++ src/NumeroAleatorio.java
@@ -1,5 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumeroAleatorio {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Scanner scanner = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(100);
+        int intentos = 0;
+        int intentosMaximo = 5;
+        boolean intentoAdivinado = false;
+
+        System.out.println("Ingrese un numero entre 0 y 100");
+        int numeroIngresado = scanner.nextInt();
+
+        System.out.println("El numero aleatorio es: " + numeroAleatorio);
+
+        while (intentos < intentosMaximo){
+            ++intentos;
+            int intentosRestantes = intentosMaximo - intentos;
+            String palabraIntentos = (intentosRestantes == 1)? "intento" : "intentos";
+
+            if (numeroIngresado == numeroAleatorio){
+                System.out.println("Lo Lograste en: " + intentos + " " + palabraIntentos);
+                intentoAdivinado = true;
+                break;
+            }else {
+                if (numeroIngresado < numeroAleatorio){
+                    System.out.println("PISTA: El número es mayor... te queda " + intentosRestantes + " " + palabraIntentos);
+                }else {
+                    System.out.println("PISTA: El número es menor... te queda " + intentosRestantes + " " + palabraIntentos);
+                }
+                if (intentosRestantes == 0){
+                    break;
+                }else {
+                    numeroIngresado = scanner.nextInt();
+                }
+            }
+        }
+
+        if (!intentoAdivinado){
+            System.out.println("Se acabaron los intentos :(");
+        }
+        scanner.close();
     }
 }
